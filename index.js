2788a1f4ac4e83f513e6519b0b21554ab063e19e
System Administrator <root@Kittipods-MacBook-Pro.local>
2018-06-28 17:36:48 +0700

    create multiple reducer

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-import { createStore } from 'redux';
+import { combineReducers,createStore } from 'redux';
+
+const userReducer = (state,action) => {
+    return state;
+}
+const tweetReducer = (state,action) => {
+    return state;
+}
+
+const reducers = combineReducers({
+    user:userReducer,
+    tweets:tweetReducer
+})
 
 const reducer = function(state,action){
     if(action.type == "NYC"){
-        return action.payload+1
+        return action.payload.id +1
     } 
     return action.payload
 
 }
 
-const store = createStore(reducer,0)//Initial state is 0
-
+const store = createStore(reducer,{
+    user:{
+        name:"Nap",
+        age:23     //This object is the state so it gonna go to be state of userReducer
+    },
+    tweets:[]  
+});
 store.subscribe(()=>{
     console.log('store change',store.getState())
 })
 
-store.dispatch({type:"NYC" ,payload:2})
+store.dispatch({type:"NYC" ,payload:2})//'payload' can change name but 'type'
 store.dispatch({type:"NY" ,payload:1})
